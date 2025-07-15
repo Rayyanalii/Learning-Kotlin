49f14457d619fe3818beda9440fc06e8d32c6651
Ayyan Ali <rayyanali2705@gmail.com>
2025-07-15 22:08:54 +0500

    Learned how to make re-usable composable components and modifiers (CSS) for each component.

--- app/src/main/java/com/example/learning_kotlin/MainActivity.kt
+++ app/src/main/java/com/example/learning_kotlin/MainActivity.kt
@@ -4,13 +4,20 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.border
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.example.learning_kotlin.ui.theme.LearningKotlinTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +27,25 @@ class MainActivity : ComponentActivity() {
         setContent {
             LearningKotlinTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Ayyan",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    Column(modifier = Modifier.padding(innerPadding)) {
+                        Greeting(
+                            name = "I am Ayyan"
+                            , modifier = Modifier.padding(bottom = 10.dp)
+                        )
+                        Card(
+                            title="Class 1",
+                            description = "This is class 1"
+                        )
+                        Card(
+                            title="Class 2",
+                            description = "This is class 2"
+                        )
+                        Card(
+                            title="Class 3",
+                            description = "This is class 3"
+                        )
+                    }
+
                 }
             }
         }
@@ -34,14 +56,16 @@ class MainActivity : ComponentActivity() {
 fun Greeting(name: String, modifier: Modifier = Modifier) {
     Text(
         text = "Hello $name!",
-        modifier = modifier
+        modifier = modifier,
+        color = Color.Blue
     )
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    LearningKotlinTheme {
-        Greeting("Android")
+fun Card(title:String,description:String){
+    Column(modifier= Modifier.padding(vertical = 8.dp, horizontal = 4.dp).border(border = BorderStroke(1.dp, color = Color.Black)).padding(all = 8.dp)) {
+        Text(text = title, fontWeight = FontWeight.Bold)
+        Text(text = description, color = Color.Gray)
     }
+
 }
